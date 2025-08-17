a274f3ff1945b642f053bb06387ddb807966a550
BIGTY <799696060@qq.com>
2025-08-17 18:34:21 +0800

    增加了一些内容

--- src/Rubbish.java
+++ src/Rubbish.java
@@ -1,6 +1,8 @@
 import java.util.Random;
 
 public class Rubbish {
+
+    //Minecraft 这一部分wiki上他奶奶的和源码不太一样，查了半天还以为是我自己的问题，我真服了！！！
     public static boolean isSlimeChunk(long worldSeed,     // 世界种子，一个64位整数，可以通过/seed获取
                                        int  chunkX,        // 区块X轴坐标，32位整数
                                        int  chunkZ) {      // 区块Z轴坐标，32位整数
